dc450c4c36d14ac3bdab351119716b32d7dee469
jules <julesgirard@laposte.net>
2023-10-04 22:38:34 +0200

    fix broken links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({  
-site: 'https://paulemile-hash.github.io',
-base: '/Portfolio',});
+export default defineConfig({
+    site: 'https://JulesGrd.github.io',
+});
