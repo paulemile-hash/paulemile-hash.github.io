f8cbc15b87ec5d61b6d7cab6615018981143d449
Paul-Émile Leroux <133578402+paulemile-hash@users.noreply.github.com>
2024-05-07 14:21:09 +0200

    new

--- src/Javascript/carousel.js
+++ src/Javascript/carousel.js
@@ -1,83 +1,22 @@
-class Carousel {
-    constructor() {
-        this.img = [];
-        this.img[0] = document.getElementById("carousel-image-0");
-        this.img[1] = document.getElementById("carousel-image-1");
-        this.img[2] = document.getElementById("carousel-image-2");
-        this.img[3] = document.getElementById("carousel-image-3");
-        this.img[4] = document.getElementById("carousel-image-4");
-        this.img[5] = document.getElementById("carousel-image-5");
+let currentSlide = 0;
 
-        this.animForward = ['mv0to5', 'mv1to0', 'mv2to1', 'mv3to2', 'mv4to3', 'mv5to4'];
-        this.animBackward = ['mv0to1', 'mv1to2', 'mv2to3', 'mv3to4', 'mv4to5', 'mv5to0'];
-
-        this.currentImage = 2;
-
-        this.reset();
-    }
-
-    reset() {
-        this.img.forEach((image) => {
-            this.animForward.forEach((animation) => {
-                image.classList.remove(animation);
-            });
-            this.animBackward.forEach((animation) => {
-                image.classList.remove(animation);
-            });
-        });
-    }
-
-    next(nextImage) {
-        if (nextImage !== undefined) this.setImage(5, nextImage);
-
-        this.img.forEach((image, i) => {
-            this.animForward.forEach((animation) => { image.classList.remove(animation); });
-            this.animBackward.forEach((animation) => { image.classList.remove(animation); });
-            image.classList.add(this.animForward[(-this.currentImage + i + 8) % 6]);
-        });
-
-        this.currentImage = (this.currentImage + 1) % 6;
-    }
-
-    previous(previousImage) {
-        // Ajoutez le code pour l'animation précédente ici
-    }
-
-    setImage(pos, src) {
-        this.img[(pos + this.currentImage + 4) % 6].src = src;
-    }
-
-    hideImage(pos) {
-        this.img[(pos + this.currentImage + 4) % 6].style.visibility = 'hidden';
-    }
-
-    showImage(pos) {
-        this.img[(pos + this.currentImage + 4) % 6].style.visibility = 'visible';
-    }
+function prev() {
+  if (currentSlide > 0) {
+    currentSlide--;
+    updateCarousel();
+  }
 }
 
-// Create an instance of the Carousel class
-const carousel = new Carousel();
-
-// Select navigation buttons
-const prevButton = document.querySelector('.prev-btn');
-const nextButton = document.querySelector('.next-btn');
-
-// Add event listeners for clicks on navigation buttons
-prevButton.addEventListener('click', () => {
-    carousel.previous();
-});
-
-nextButton.addEventListener('click', () => {
-    carousel.next();
-});
-
-next() 
-{
-    const nextImage = this.currentImage === 5 ? 0 : this.currentImage + 1;
-    const nextSrc = this.img[nextImage].src;
-    this.img.forEach((image, i) => {
-        image.src = this.img[(nextImage + i) % 6].src;
-    });
-    this.currentImage = nextImage;
+function next() {
+  if (currentSlide < document.querySelectorAll('.carousel-item').length - 1) {
+    currentSlide++;
+    updateCarousel();
+  }
 }
+
+function updateCarousel() {
+  const carouselInner = document.querySelector('.carousel-inner');
+  carouselInner.style.transform = `translateX(-${currentSlide * 100}%`;
+}
+
+updateCarousel();
